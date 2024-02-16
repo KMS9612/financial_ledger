0317e76f30dceff02090b275400d0559d3b45180
KMS9612 <kimdev9612@gmail.com>
2024-02-16 16:18:03 +0900

    useCheckLogin git add 후 수정한 부분 재 커밋

--- app/functions/checkLogin.tsx
+++ app/functions/checkLogin.tsx
@@ -4,13 +4,11 @@ import PleaseLogin from "../src/components/modals/plsLogin";
 
 export default function useCheckLogin(Component: ComponentType) {
   return function ProtectedRoute({ ...props }) {
-    const [isLogin, setIsLogin] = useState(
-      sessionStorage.getItem("access") || ""
-    );
+    const [isLogin, setIsLogin] = useState("accessToken");
 
     useEffect(() => {
       setIsLogin(sessionStorage.getItem("access") || "");
-    });
+    }, []);
 
     // 로그인이 되어 있지 않다면 로딩 화면을 보여줍니다.
     if (!isLogin) {
