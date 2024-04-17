bbbd867279c72e4f1eeff1fe0b4bd569ccb60841
KMS9612 <kimdev9612@gmail.com>
2024-04-17 17:46:33 +0900

    EditPage TodayPayModal에서 일일정보등록 요청 시 버튼을 여러번 누르면 동일한 정보가 여러번 등록되던 문제 boolean상태를 사용하여 한 번 클릭시 로딩창이 나오며 버튼이 disabled 되도록 수정 완료

--- app/src/components/modals/todayPayModal.tsx
+++ app/src/components/modals/todayPayModal.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import { IPropsTodayModal } from "../../types/modalTypes/todayPayModalType";
 import api from "@/app/axios/instance";
+import CircleLoading from "../loading/circleLoading";
 
 export default function TodayPayModal({
   isOpenFunction,
@@ -9,6 +10,8 @@ export default function TodayPayModal({
   formData,
   fetchTableData,
 }: IPropsTodayModal) {
+  // 연속 클릭 방지 및 로딩 상태 파악용 State
+  const [isRequest, setIsRequest] = useState<boolean>(false);
   // Edit에 필요한 Input의 정보를 모아놓은 객체
   const inputObj = [
     {
@@ -36,8 +39,6 @@ export default function TodayPayModal({
   };
 
   const onClickSendRequest = async () => {
-    // 23.12.25 추후 연달아 클릭하는 거 방지하는 기능 필요함
-
     if (
       !formData.date ||
       !formData.place ||
@@ -47,6 +48,10 @@ export default function TodayPayModal({
       alert("입력내용을 모두 입력해 주세요");
       return;
     }
+
+    // 23.12.25 추후 연달아 클릭하는 거 방지하는 기능 필요함
+    // 24.04.17 연속 클릭 방지 추가
+    setIsRequest(true);
 
     const email = sessionStorage.getItem("email");
     await api
@@ -65,6 +70,7 @@ export default function TodayPayModal({
         console.log(err);
       });
     isOpenFunction("today", false);
+    setIsRequest(false);
     fetchTableData();
   };
 
@@ -75,7 +81,7 @@ export default function TodayPayModal({
           ? "opacity-100  pointer-events-auto"
           : "opacity-0  pointer-events-none"
       }
-      absolute w-screen min-w-[380px] lg:w-96 h-fit py-10 bg-white border-slate-700 border-2 top-1/2 left-1/2 w-1/5 -translate-x-1/2 -translate-y-1/2 text-center transition ease-in-out rounded z-10`}
+      absolute min-w-[380px] lg:w-96 h-fit py-10 bg-white border-slate-700 border-2 top-1/2 left-1/2 w-1/5 -translate-x-1/2 -translate-y-1/2 text-center transition ease-in-out rounded z-10`}
     >
       <div className="mb-10 text-lg font-bold">오늘 지출 등록</div>
       <div className="flex flex-col justify-center items-center gap-10">
@@ -130,10 +136,13 @@ export default function TodayPayModal({
         })}
         <div className="flex flex-col gap-5">
           <button
+            disabled={isRequest}
             onClick={onClickSendRequest}
-            className="w-80 lg:w-60 h-10 border rounded"
+            className={`${
+              isRequest && "bg-gray-300 cursor-wait"
+            } w-80 lg:w-60 h-10 flex justify-center items-center border rounded`}
           >
-            저장
+            {isRequest ? <CircleLoading /> : "저장"}
           </button>
           <button
             className="w-80 lg:w-60 h-10 border rounded"
