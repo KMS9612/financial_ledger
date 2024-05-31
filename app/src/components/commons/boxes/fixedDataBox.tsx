9e7071348e9fb63c71fe15c631071b3072487bf9
KMS9612 <kimdev9612@gmail.com>
2024-05-31 11:10:50 +0900

    editDetail페이지의 고정데이터를 보여주는 fixedDataBox컴포넌트 내부의 api요청부분 및 view부분을 커스텀훅과 array.map을 사용해 가시성 좋고 유지보수하기 좋게 수정

--- app/src/components/commons/boxes/fixedDataBox.tsx
+++ app/src/components/commons/boxes/fixedDataBox.tsx
@@ -1,37 +1,47 @@
-import api from "@/app/src/service/instance";
-import { useEffect, useState } from "react";
-import { IFixedData } from "../../../types/fixedTypes/fixedDataType";
+import { FixedDataTypeGuard } from "@/app/src/types/fixedTypes/fixedDataType";
+import useFinancailData from "@/app/src/lib/hooks/useFinancailData";
+import TextSkeleton from "../../loading/textSkeleton";
 
 export default function FixedDataBox() {
-  const [fixedData, setFixedData] = useState<IFixedData>({
-    _id: "",
-    email: "",
-    saving: 0,
-    fixed: 0,
-    income: 0,
-  });
+  const { fixedData } = useFinancailData();
 
-  // 고정지출, 고정수입, 저금 내용 불러온 후 state에 저장
-  useEffect(() => {
-    const getFixed = async () => {
-      const email = sessionStorage.getItem("email");
-      const params = { email };
-      const resData = await api.get("/fix/fetchFixedData", { params });
-      setFixedData(resData.data.fixedData);
-    };
-    getFixed();
-  }, []);
+  const dataMap = [
+    {
+      innerText: "고정수입",
+      dataField: !FixedDataTypeGuard(fixedData) ? (
+        <TextSkeleton />
+      ) : (
+        fixedData.income
+      ),
+    },
+    {
+      innerText: "고정지출",
+      dataField: !FixedDataTypeGuard(fixedData) ? (
+        <TextSkeleton />
+      ) : (
+        fixedData.fixed
+      ),
+    },
+    {
+      innerText: "저금",
+      dataField: !FixedDataTypeGuard(fixedData) ? (
+        <TextSkeleton />
+      ) : (
+        fixedData.saving
+      ),
+    },
+  ];
+
   return (
     <div className="flex flex-row flex-wrap gap-2 items-center">
-      <div className="border-2 border-slate-600 font-bold rounded-md h-10 flex justify-center items-center px-10 text-xs lg:text-base min-w-full xl:min-w-[215px]">
-        고정수입: {fixedData?.income} 원
-      </div>
-      <div className="border-2 border-slate-600 font-bold rounded-md h-10 flex justify-center items-center px-10 text-xs lg:text-base min-w-full xl:min-w-[215px]">
-        고정지출: {fixedData?.fixed} 원
-      </div>
-      <div className="border-2 border-slate-600 font-bold rounded-md h-10 flex justify-center items-center px-10 text-xs lg:text-base min-w-full xl:min-w-[215px]">
-        저금: {fixedData?.saving} 원
-      </div>
+      {dataMap.map((el) => (
+        <div
+          key={el.innerText}
+          className="border-2 border-slate-600 font-bold rounded-md h-10 flex justify-center items-center px-10 text-xs lg:text-base min-w-full xl:min-w-[215px]"
+        >
+          {el.innerText}: {el.dataField} 원
+        </div>
+      ))}
     </div>
   );
 }
