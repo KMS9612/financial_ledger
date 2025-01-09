0885c5143b0958ed0187106ed493d890f44cf6df
KMS9612 <kimdev9612@gmail.com>
2025-01-09 23:01:38 +0900

    Delete Test DB Info And ReWire New DB INFO with Env

--- app/src/lib/db/db.ts
+++ app/src/lib/db/db.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "financial_ledger",
-  password: "rlaalstmd12!",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
 });
 
 export { pool };
